471874bd16da62eda0622bdcc77432c48513e919
Suronjit pal <suronjit797@gmail.com>
2022-06-01 00:30:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {
+          Array.isArray(users) && users.map((user, index) => (
+            <p key={index}> {user.name} </p>
+          ))
+        }
+      </div>
+      export default Bug;
     </div>
   );
 }
